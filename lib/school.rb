d7d217080a826b629ec97c5d29ddab30b0a5770d
Levi Borland <borland.levi@gmail.com>
2019-08-30 17:02:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,17 +1,30 @@
 class School
-  
-  attr_accessor :roster, :name, :grade
-  attr_reader :school_name
 
   def initialize(school_name)
     @school_name = school_name
     @roster = {}
   end
-  
-  def add_student(student_name, grade)
-    @roster[grade] = []
-    @roster[grade] << student_name
+
+  def roster
+    @roster
   end
 
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
 
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
 end
