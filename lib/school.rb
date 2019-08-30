4388c84c35bbbde47e077b71ee91bbb9d29db3eb
Levi Borland <borland.levi@gmail.com>
2019-08-30 16:42:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,6 +6,10 @@ class School
     @school_name = school_name
     @roster = {}
   end
+  
+  def add_student(student_name, grade)
+    @roster[grade] = []
+    @roster[grade] << student_name
 
 
 end
